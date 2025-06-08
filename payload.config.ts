63db234c259bd0af88f0c8d8d154ab1f754373db
kismatkc <52825603+kismatkc@users.noreply.github.com>
2025-06-08 01:44:07 +0000

    treks

--- payload.config.ts
+++ payload.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { s3Storage } from '@payloadcms/storage-s3' // ✅ named export
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -16,6 +17,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 
+  
 export default buildConfig({
   serverURL: process.env.SERVER_URL,
   admin: {
@@ -42,5 +44,18 @@ export default buildConfig({
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
+    s3Storage({
+      collections: { media: true },
+      bucket: process.env.S3_BUCKET as string,
+      config: {
+        endpoint: process.env.S3_ENDPOINT,
+         region: 'ap-south-1', // any string works; Filebase ignores it
+        forcePathStyle: true, // mandatory for non‑AWS hosts
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+          secretAccessKey: process.env.S3_SECRET as string,
+        },
+      },
+    }),
   ],
 })
